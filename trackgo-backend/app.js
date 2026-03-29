d7a8f631962b420fc7ff5148a532bacf1ef21dbe
maheshkakad9 <maheshkakad5678@gmail.com>
2026-03-29 10:56:46 +0530

    feat: add initial database schema for company, users, expenses, approvals

--- trackgo-backend/app.js
+++ trackgo-backend/app.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./modules/auth/auth.routes'));
+// app.use('/api/auth', require('./modules/auth/auth.routes'));
 
 module.exports = app;
